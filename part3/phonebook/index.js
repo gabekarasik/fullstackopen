35728b5b0f144cc9d234cfdbbd36caa5385e64c0
Gabe Karasik <gabriel.karasik1@gmail.com>
2026-04-10 13:43:39 -0400

    Exercises 3.7 to 3.8

--- part3/phonebook/index.js
+++ part3/phonebook/index.js
@@ -1,7 +1,13 @@
 const express = require('express')
+const morgan = require('morgan')
 const app = express()
 
+morgan.token('body', function getBody (request) {
+  return JSON.stringify(request.body)
+})
+
 app.use(express.json())
+app.use(morgan(':method :url :status :res[content-length] - :response-time ms :body'))
 
 let persons = [
     { 
