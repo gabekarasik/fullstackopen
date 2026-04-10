df2b732d4b04f967acf902769603a971f67dd1cd
Gabe Karasik <gabriel.karasik1@gmail.com>
2026-04-10 18:44:25 -0400

    notes backend 3b

--- part3/examples/template/index.js
+++ part3/examples/template/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const app = express()
+const cors = require('cors')
 
 const requestLogger = (request, response, next) => {
   console.log('Method:', request.method)
@@ -10,7 +11,8 @@ const requestLogger = (request, response, next) => {
 }
 
 app.use(express.json())
-app.use(requestLogger())
+app.use(cors())
+app.use(requestLogger)
 
 let notes = [{
     id: "1",
@@ -86,7 +88,7 @@ const unknownEndpoint = (request, response) => {
 
 app.use(unknownEndpoint)
 
-const PORT = 3001
+const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
 })
